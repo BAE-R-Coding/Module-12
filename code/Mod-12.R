c85387ebb7a6bf1419c60a87979648cb7e7d414b
Lucas Mitchell <lcmitch3@ncsu.edu>
2019-11-18 22:33:05 -0500

    Done 1,2,3 4 needs help

--- code/Mod-12.R
+++ code/Mod-12.R
@@ -1,9 +1,9 @@
 #Lucas Mitchell
 #11/12/2019
 
-# install.packages("wordcloud")
-# install.packages("textdata")
-# install.packages("tidytext")
+ install.packages("wordcloud")
+ install.packages("textdata")
+ install.packages("tidytext")
 
 # clear workspace
 rm(list=ls(all=TRUE))
@@ -25,4 +25,51 @@ ggplot(tw)+
   geom_vline(xintercept = ymd_hms("2017-08-26 3:00:00"), color = "red")+
   theme_bw()
 
+#------------------------------------------------------------------------
 
+
+new_stops <- c("hurricane", "harvey", "hurricaneharvey", "http", "https", "html", "ift.tt", "pic.twitter.com", "twitter.com", "fb.me", "bit.ly", "dlvr.it", "youtube", "youtu.be")
+new_stop <- stop_words %>%
+  add_row(word = new_stops )
+
+word <- tweet%>%
+  unnest_tokens(word, tweet)%>%
+  anti_join(new_stop)%>%
+  count(word, sort = TRUE)%>%
+  top_n(20)%>%
+  mutate(word = reorder(word,n))
+
+ggplot(word)+
+  geom_col(mapping = aes(word, n))+
+  coord_flip()
+
+
+#---------------------------------------------------------------------------
+
+
+ref_tweet <- tweet%>%
+  filter(str_detect(tweet, c("refinery", "refineries")))
+
+ref_tweet%>%
+  unnest_tokens(word, tweet)%>%
+  anti_join(new_stop)%>%
+  count(word, sort = TRUE)%>%
+  with(wordcloud(word, n, max.words = 100))
+
+
+#---------------------------------------------------------------------------
+
+get_sentiments("afinn")
+
+f_tweet <- tweet %>%
+  group_by(date)%>%
+  unnest_tokens(word, tweet)%>%
+  count(word, sort = TRUE)
+
+tweet_sent <- f_tweet%>%
+  inner_join(get_sentiments("afinn"))%>%
+  summarize(mean = mean(value))
+
+tweet_sent %>%
+  ggplot()+
+  geom_col(mapping = aes(date, mean))
